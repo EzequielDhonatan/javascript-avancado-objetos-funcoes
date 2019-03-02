9cf596a4932ea58f270c37694476172f55d29d48
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-03-02 19:17:28 -0300

    Objetos em JavaScript

---
+++ app.js
@@ -0,0 +1,32 @@
+var ezequiel = {
+    name: 'Ezequiel',
+    lastName: 'Dhonatan',
+
+    fullName: function () {
+        return `${this.name} - ${this.lastName}`
+    }
+}
+
+console.log(ezequiel.fullName());
+
+var outro = {
+    name: 'Outro',
+    lastName: 'Dhonatan',
+
+    fullName: function () {
+        return `${this.name} - ${this.lastName}`
+    }
+}
+
+console.log(outro.fullName());
+
+var outro2 = {
+    name: 'Outro 2',
+    lastName: 'Dhonatan',
+
+    fullName: function () {
+        return `${this.name} - ${this.lastName}`
+    }
+}
+
+console.log(outro2.fullName());
